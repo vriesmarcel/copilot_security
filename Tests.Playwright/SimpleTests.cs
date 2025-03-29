5b2f63be60bf0713c406f7ed38ea898f9583e0dd
Marcel de Vries <vriesmarcel@hotmail.com>
2025-03-29 23:57:33 +0100

    make methods Async

--- Tests.Playwright/SimpleTests.cs
+++ Tests.Playwright/SimpleTests.cs
@@ -8,19 +8,19 @@ namespace Tests.Playwright
         public string StartPage = "https://globoticket-frontend-dpfbe7hxa6d2bdab.westeurope-01.azurewebsites.net/";
 
         [Test] 
-        public void BuyOneProduct()
+        public async Task BuyOneProduct()
         {
             //insert Generated code here
         }
 
         [Test]
-        public void BuyTwoProducts()
+        public async Task BuyTwoProducts()
         {
             //insert Generated code here
         }
 
         [Test]
-        public void SelectTreeTicketsAndRemoveOne()
+        public async Task SelectTreeTicketsAndRemoveOne()
         {
             //insert Generated code here
         }
