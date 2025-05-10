b43c6c3b9aa0b2ef8673133bf0f9bd46e8109db5
Marcel de Vries <vriesmarcel@hotmail.com>
2025-05-10 20:50:54 +0200

    adding login controller

---
+++ frontend/Controllers/LogInController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace GloboTicket.Frontend.Controllers
+{
+    public class LoginController(SqlConnection connection) : Controller
+    {
+        private readonly SqlConnection _connection = connection;
+        [HttpGet("login")]
+        public IActionResult Login(string username, string password) 
+        {
+
+        string query = 
+        $"SELECT * FROM Users WHERE Username = '{username}' AND Password = '{password}'";
+
+        using var command = 
+                new SqlCommand(query, _connection);
+
+        using var reader = command.ExecuteReader();
+
+        if (reader.HasRows)
+        {
+            return Ok("Login successful");
+        }
+
+        return Unauthorized("Invalid credentials");
+        }
+    }
+}
